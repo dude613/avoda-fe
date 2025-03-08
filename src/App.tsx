32706d449d1adca3bd38dfa42771ccc19310cf45
itsmenavi38 <itsmenavi38@gmail.com>
2025-03-08 20:05:26 +0530

    Set up frontend and create project layout

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-500'>
+        <h1 className='text-green-500'>Welcome</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
