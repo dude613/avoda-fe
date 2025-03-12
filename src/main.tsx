08488c41176aee2f86876a84c85cf6950da00336
Avi <liamswift2014@gmail.com>
2025-03-12 00:43:08 -0400

    Add Sentry integration for error monitoring and tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
+import * as Sentry from "@sentry/react";
+import { browserTracingIntegration, replayIntegration } from "@sentry/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
+
+Sentry.init({
+  dsn: "https://32a372d399967c40e175a081bbb13db9@sen.newhoopla.com/4",
+  integrations: [
+    browserTracingIntegration(),
+    replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    })
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
